07ebe1f7472efabb6cdb07b7f310a9aef8d61399
Yazan Alhusamia <sadprincegost@gmail.com>
2022-01-04 09:36:12 +0200

    add the message and the waver to the cont.

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,19 +1,38 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
+pragma experimental ABIEncoderV2;
 
 import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
 
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address from;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave[] waves;
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s has waved!", msg.sender);
+        console.log("%s has waved!", msg.sender, _message);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
