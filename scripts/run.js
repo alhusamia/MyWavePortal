64049617eb5d4a430ffeb3fb76e0b8c89a927fe2
Yazan Alhusamia <sadprincegost@gmail.com>
2022-01-05 10:38:51 +0200

    apply a random number

--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,9 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy();
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
   await waveContract.deployed();
 
   console.log("Contract deployed to:", waveContract.address);
@@ -18,19 +20,17 @@ const main = async () => {
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
 
-  let waveTxn = await waveContract.wave("A message");
+  const waveTxn = await waveContract.wave("This is wave #1");
   await waveTxn.wait();
+
+  const waveTxn2 = await waveContract.wave("This is wave #2");
+  await waveTxn2.wait();
 
   contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
   console.log(
     "Contract balance:",
     hre.ethers.utils.formatEther(contractBalance)
   );
-
-  waveCount = await waveContract.getTotalWaves();
-
-  waveTxn = await waveContract.connect(randomPerson).wave("another message");
-  await waveTxn.wait();
 
   waveCount = await waveContract.getTotalWaves();
 
